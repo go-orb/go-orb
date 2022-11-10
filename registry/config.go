7206095bfb5ea651d4d4c097f71d49d66535dd12
Davincible <david.brouwer.99@gmail.com>
2022-11-10 19:07:42 +0100

    feat(registry): add functional interface options

--- registry/config.go
+++ registry/config.go
@@ -13,7 +13,36 @@ var (
 	DefaultTimeout  = 600
 )
 
+var _ (ConfigType) = (*Config)(nil)
+
+// Option is a functional option type for the registry.
+type Option func(ConfigType)
+
+// ConfigType is used in the functional options as type to identify a registry
+// option. It is used over a static *Config type as this way plugins can also
+// easilty set functional options without the complication of contexts, as was
+// done in v4. This is possible because plugins will nest the registry.Config
+// type, and thus inherit the interface that is used to identify the registry
+// config.
+//
+// Plugin specific option example:
+//
+//		 // WithLogger option located in the MDNS registry package.
+//			func WithLogger(logger log.Logger) registry.Option {
+//			 	return func(c registry.ConfigType) {
+//	        // The config type used here is *mdns.Config
+//			 	   cfg, ok := c.(*Config)
+//			 	   if ok {
+//			 	    	cfg.Logger = logger
+//			 	   }
+//			 	}
+//			}
+type ConfigType interface {
+	config() *Config
+}
+
 func init() {
+	// Register registry CLI flags.
 	err := cli.Flags.Add(cli.NewFlag(
 		"registry",
 		DefaultRegistry,
@@ -44,6 +73,26 @@ type Config struct {
 	Plugin  string     `json:"plugin,omitempty" yaml:"plugin,omitempty"`
 	Timeout int        `json:"timeout,omitempty" yaml:"timeout,omitempty"`
 	Logger  log.Logger `json:"logger,omitempty" yaml:"logger,omitempty"`
+}
+
+func (c *Config) config() *Config {
+	return c
+}
+
+// WithTimeout sets the default registry timeout used.
+func WithTimeout(timeout int) Option {
+	return func(cfg ConfigType) {
+		c := cfg.config()
+		c.Timeout = timeout
+	}
+}
+
+// WithLogger sets a specific logger to use.
+func WithLogger(logger log.Logger) Option {
+	return func(cfg ConfigType) {
+		c := cfg.config()
+		c.Logger = logger
+	}
 }
 
 // NewConfig creates a new default config to use with a registry.
--- registry/plugins.go
+++ registry/plugins.go
@@ -7,9 +7,6 @@ import (
 	"go-micro.dev/v5/util/container"
 )
 
-// Option is a functional option type for the registry.
-type Option func(*Config)
-
 // Provider is provider function type used by plugins to create a new registry.
 type Provider func(name types.ServiceName, data []source.Data, logger log.Logger, opts ...Option) (*MicroRegistry, error)
 
