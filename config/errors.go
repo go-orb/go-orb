71504dd1049834b6323f8808b23a96819e9e3acc
Davincible <david.brouwer.99@gmail.com>
2022-11-15 02:52:26 +0100

    fix(config): add helper hint to error messages

--- config/errors.go
+++ config/errors.go
@@ -4,7 +4,7 @@ import "errors"
 
 var (
 	// ErrUnknownPlugin happens when there's no config factory for the given plugin.
-	ErrUnknownPlugin = errors.New("unknown config given")
+	ErrUnknownPlugin = errors.New("unknown config given. Did you import the config plugin?")
 
 	// ErrNotExistent happens when a config key is not existent.
 	ErrNotExistent = errors.New("no such config key")
@@ -13,8 +13,8 @@ var (
 	ErrTypesDontMatch = errors.New("config key requested type and actual type don't match")
 
 	// ErrUnknownScheme happens when you didn't import the plugin for the scheme or the scheme is unknown.
-	ErrUnknownScheme = errors.New("unknown config source scheme")
+	ErrUnknownScheme = errors.New("unknown config source scheme. Did you register the config source plugin for your scheme?")
 
 	// ErrNoSuchFile happens when theres no file.
-	ErrNoSuchFile = errors.New("no such file or no marshaler found")
+	ErrNoSuchFile = errors.New("no such file or no marshaler found. Did you import the codec plugin for your file type?")
 )
