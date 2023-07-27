76f48d13ed1f84c4bb25db6ca895d4697da98c5b
René Jochum <rene@jochum.dev>
2023-07-28 00:06:42 +0200

    fix(codecs): No need for nolint

--- codecs/errors.go
+++ codecs/errors.go
@@ -2,8 +2,7 @@ package codecs
 
 import "errors"
 
-//nolint:gochecknoglobals
 var (
-	// ErrUknownMimeType happens when you request a codec for a unknown mime type.
+	// ErrUnknownMimeType happens when you request a codec for a unknown mime type.
 	ErrUnknownMimeType = errors.New("unknown mime-type given")
 )
