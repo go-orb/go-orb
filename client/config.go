cd243523667d739d557101a0f1229a7acf41aab0
René Jochum <jochumdev@gmail.com>
2025-02-23 10:06:27 +0100

    feat(client): Optimize the clients config

--- client/config.go
+++ client/config.go
@@ -16,7 +16,7 @@ var (
 	// DefaultContentType is the default Content-Type for calls.
 	DefaultContentType = "application/x-protobuf"
 	// DefaultPreferredTransports set's in which order a transport will be selected.
-	DefaultPreferredTransports = []string{"http", "grpc", "h2c", "http2", "http3", "https"}
+	DefaultPreferredTransports = []string{"grpc", "drpc", "http", "h2c", "http2", "http3", "https"}
 
 	// DefaultPoolHosts set the number of hosts in a pool.
 	DefaultPoolHosts = 16
@@ -437,7 +437,7 @@ func WithTLSConfig(n *tls.Config) CallOption {
 	}
 }
 
-// WithResponseMetadata will be written into the value when giving.
+// WithResponseMetadata will write response Metadata into the give map.
 func WithResponseMetadata(n map[string]string) CallOption {
 	return func(o *CallOptions) {
 		o.ResponseMetadata = n
