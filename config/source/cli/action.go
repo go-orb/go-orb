7678bb66edada960eb30ac5d4fa528108fff2532
René Jochum <jochumdev@gmail.com>
2025-02-28 16:00:29 +0100

    fix: lint errors

--- config/source/cli/action.go
+++ config/source/cli/action.go
@@ -1,15 +1,18 @@
 package cli
 
+// SelectedAction is a helper struct to hold the service name and the action when an cli app chooses it.
 type SelectedAction struct {
 	ServiceName string
 	Action      string
 }
 
+// Set updates its values.
 func (s *SelectedAction) Set(sName string, action string) {
 	s.ServiceName = sName
 	s.Action = action
 }
 
+// String returns a human readable string.
 func (s *SelectedAction) String() string {
 	return s.ServiceName + ": " + s.Action
 }
