12ea48bd3075c76ce3828bbb47b3ce8ab67f7d9c
René Jochum <jochumdev@gmail.com>
2025-03-21 01:19:56 +0100

    feat(config): Add more mime types.

--- codecs/mime.go
+++ codecs/mime.go
@@ -5,3 +5,12 @@ const MimeJSON = "application/json"
 
 // MimeProto is the mime type for protocol buffer.
 const MimeProto = "application/x-protobuf"
+
+// MimeYAML is the mime type for YAML.
+const MimeYAML = "application/yaml"
+
+// MimeTOML is the mime type for TOML.
+const MimeTOML = "application/toml"
+
+// MimeXML is the mime type for XML.
+const MimeXML = "application/xml"
