3e2a0f6ba13b60d07a05893220134f16507a4244
René Jochum <jochumdev@gmail.com>
2025-03-22 03:17:06 +0100

    feat(config): Add URL to config.

--- config/special_types.go
+++ config/special_types.go
@@ -3,6 +3,8 @@ package config
 import (
 	"encoding/json"
 	"errors"
+	"net/url"
+	"strings"
 	"time"
 )
 
@@ -40,4 +42,59 @@ func (d *Duration) UnmarshalJSON(b []byte) error {
 	default:
 		return errors.New("invalid duration")
 	}
+}
+
+// URL represents a URL in JSON.
+type URL struct {
+	// URL is the underlying URL.
+	*url.URL
+}
+
+// String returns the string representation of the JSONURL.
+func (j *URL) String() string {
+	if j == nil || j.URL == nil {
+		return ""
+	}
+
+	return j.URL.String()
+}
+
+// Copy returns a copy of the JURL.
+func (j *URL) Copy() (*URL, error) {
+	if j == nil || j.URL == nil {
+		return nil, errors.New("JURL is nil")
+	}
+
+	return NewURL(j.URL.String())
+}
+
+// NewURL creates a new `json` URL.
+func NewURL(s string) (*URL, error) {
+	u, err := url.Parse(s)
+	if err != nil {
+		return nil, err
+	}
+
+	return &URL{URL: u}, nil
+}
+
+// UnmarshalJSON unmarshals the JURL from JSON.
+func (j *URL) UnmarshalJSON(data []byte) error {
+	u, err := url.Parse(strings.Trim(string(data), `"`))
+	if err != nil {
+		return err
+	}
+
+	j.URL = u
+
+	return nil
+}
+
+// MarshalJSON marshals the JURL to JSON.
+func (j *URL) MarshalJSON() ([]byte, error) {
+	if j == nil || j.URL == nil {
+		return nil, nil
+	}
+
+	return []byte(`"` + j.URL.String() + `"`), nil
 }
